6ec9ab80a983f3df07efe2f09a94da22d90d3757
Gang Liang <gang.liang.2011@gmail.com>
2015-11-22 18:26:22 -0500

    commit modified file?

--- DirTree/DirTree.pl
+++ DirTree/DirTree.pl
@@ -1,39 +1,17 @@
 use strict;
 use warnings;
 
+use File::Basename;
+
 use Cwd;
 use FindBin qw($Bin);
 use lib "$Bin/lib";
 
 use DirTree;
+use DirFormat;
 
 my $cwd = getcwd();
+my @splits = split(/\//, $cwd);
 
-# subs to get dirs/files/depth
-my @lists = get_lists($cwd);
-
-# print without formatting
-my $obj = DirTree->new($cwd,1,\@lists);
-
-my $dir = $obj->get_directory();
-my $depth = $obj->get_depth();
-
-my $files_aref = $obj->get_files();
-foreach my $file(@$files_aref){
-    chomp($file);
-    if(-d $file){
-        print "recursive_dir($file)\n";
-    }else{
-        print "formating($file)\n";
-    }
-}
-
-sub get_lists{
-    my $dir = shift;
-    my @lists = `find $dir -maxdepth 1`;
-
-    return @lists;
-}
-# formating - skip
-
+recursive_dir($cwd, scalar(@splits) );
 
