5cca2b947e9689e4fb6e2dbc4740dc26bda12820
Gang Liang <gang.liang.2011@gmail.com>
2015-12-23 20:54:26 -0500

    add python e

---
+++ foundation/python_tools/lib/Restaurant.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import re
+
+class RestaurantName():
+
+    def __init__(self, input_contents):
+        self.name = input_contents
+        if re.match("^[A-Za-z0-9_-]*$",self.name):
+            self.no_space_name = self.name.replace(' ', '')
+            if str.isalpha(self.no_space_name):
+                pass
+            else:
+                raise ValueError('There are non alphabetic characters that I can not recognize!')
+
+        else:
+            raise TypeError('Not String! The input is supposed to be a string type!')
+
+    def __repr__(self):
+        return 'RestaurantName(%s)' % self.name.strip()  
+
+    def __str__(self):
+        return self.name.strip() 
+
+
+
---
+++ foundation/python_tools/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pythonEx',
+    version='0.1',
+    description='python tools',
+    url='https://github.com/EverydayQA',
+    author='Gang Liang',
+    author_email='gang.liang.2011@gmail.com',
+    license='MIT',
+    packages=['python_tools'],
+    zip_safe=False)
+
