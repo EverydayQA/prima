16e8c2b5f5a91af05b23f3d9b723276fcc218789
Gang Liang <gang.liang.2011@gmail.com>
2016-01-20 21:25:10 -0500

    mock

---
+++ python_tools/tests/ex0.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import mock
+import unittest
+
+
+def check_method_return(input):
+    return_value = input.ops.list()
+
+    if not return_value:
+        return False
+
+    return return_value
+
+
+def check_method_len(input):
+
+    return_value = input.ops.list()
+
+    if len(return_value) < 1:
+        return False
+
+    return return_value
+
+class TestMockReturnValue(unittest.TestCase):
+
+    def test_mock_return(self):
+        fake_input = mock.MagicMock()
+        fake_input.ops.list.return_value = []
+
+        result = check_method_return(fake_input)
+        self.assertFalse(result)
+
+
+    def test_mock_len(self):
+        fake_input = mock.MagicMock()
+        fake_input.ops.list.return_value = []
+
+        result = check_method_len(fake_input)
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    test_empty = []
+    if not test_empty:
+        print("empty list equals to False")
+
+    unittest.main()
+
---
+++ python_tools/tests/ex1.py
@@ -0,0 +1,33 @@
+import mock
+import unittest
+import ex1
+
+def check_method_return(input):
+    return_value = input.ops.list()
+    if not return_value:
+        return False
+    return return_value
+
+def check_method_len(input):
+    return_value = input.ops.list()
+    if len(return_value) < 1:
+        return False
+    return return_value
+
+class TestMockReturnValue(unittest.TestCase):
+    def test_mock_input(self):
+        fake_input = mock.MagicMock(name='input')
+        fake_input.ops.list.return_value = []
+        result = check_method_return(fake_input)
+        self.assertFalse(result)
+
+    def test_mock_len(self):
+        fake_input = mock.MagicMock()
+        fake_input.ops.list.return_value = []
+        result = check_method_len(fake_input)
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMockReturnValue)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
