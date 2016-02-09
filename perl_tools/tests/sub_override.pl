3bb5230dd57fa6845df520ba6938b45ad33efb79
Gang Liang <gang.liang.2011@gmail.com>
2016-02-08 19:41:38 -0500

    sub override

--- perl_tools/tests/sub_override.pl
+++ perl_tools/tests/sub_override.pl
@@ -1,9 +1,19 @@
 #!/usr/bin/perl
 use Test::More;
 use Sub::Override;
+use FindBin qw($Bin);
+use lib "${Bin}/../lib";
+use Arithmetic;
+
+# not working
+my $ar = Arithmetic::add(2,3);
+
 {
-    my $override = Sub:;Override->new(
-        'Weird::Legacy::Dependency::rnd_salutation', sub {'Hi'});
-    is Foo::hello('Joe'), 'Hi, Joe', 'Say hi to Joe';
+    my $override = Sub::Override->new(
+        'add', sub {'Hi'; return 11;}
+    );
+    my $addition = $ar;
+    ok($addition == 11, "expect 11, got: $addition");
 
 }
+done_testing();
