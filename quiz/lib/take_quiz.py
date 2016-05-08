25e652e7b4e45042da72258f2b5af990376200b1
Gang Liang <gang.liang.2011@gmail.com>
2016-05-08 13:34:46 -0400

    args in unittest

--- quiz/lib/take_quiz.py
+++ quiz/lib/take_quiz.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+import logging
+import menu
+import quiz
+# args - debugging level 
+# logging inside class
+
 
 class TakeQuiz(object):
     def __init__(self, category):
@@ -8,7 +14,7 @@ class TakeQuiz(object):
 def main():
     # choose category
     categories = ['QC', 'python']
-    category = select_from_list(categories, 'Please choose a category')
+    category = menu.select_from_list(categories, 'Please choose a category')
 
     take_quiz = TakeQuiz(category)
     file_question = take_quiz.file_to_read(category)
@@ -16,10 +22,9 @@ def main():
     data_list = take_quiz.read_question(file_question)
     sels = take_quiz.select(data_list)
 
-    # save answers
+    # save answers - an Answer base class?
     take_quiz.save_sels(sels)
 
-    # judgement call? another class?
 
 
 if __name__ == '__main__':
---
+++ quiz/tests/test_quiz.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import unittest
+import os
+import sys
+import mock
+import argparse
+
+pwd = os.path.dirname(os.path.realpath(__file__))
+base_dir = os.path.join(pwd,'..')
+sys.path.append(base_dir)
+
+# demo usage of args in unittest
+
+from lib import menu
+from lib import color_print
+
+class QuizTest(unittest.TestCase):
+    def __init__(self, methodName='runTest', args=None):
+        super(QuizTest, self).__init__(methodName)
+        self.args = args
+
+class QuizQATest(QuizTest):
+    def test_if_verbose(self):
+        if self.args.verbose:
+            self.assertTrue(self.args.verbose, True)
+        else:
+            self.assertFalse(self.args.verbose)
+
+class QuizPythonTest(QuizTest):
+    def test_pass_anything(self):
+        self.assertTrue(self.args.verbose)
+    
+
+def make_suite(testcase_class, args):
+    testloader = unittest.TestLoader()
+    testnames = testloader.getTestCaseNames(testcase_class)
+    suite = unittest.TestSuite()
+    for name in testnames:
+        suite.addTest(testcase_class(name, args=args))
+    return suite
+
+def init_args_unittest():    
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-?", '--help', action="help",help='xxx')
+    parser.add_argument("-v", '--verbose', action='store_true', dest='verbose', help='verbose')
+    parser.add_argument('files', nargs='*')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = init_args_unittest()
+    print args
+
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(make_suite(QuizTest, args))
+    test_suite.addTest(make_suite(QuizQATest, args))
+    test_suite.addTest(make_suite(QuizPythonTest, args))
+    result = unittest.TextTestRunner(verbosity=2).run(test_suite)
+    sys.exit(not result.wasSuccessful())
