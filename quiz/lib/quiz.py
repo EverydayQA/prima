f50b22557d5744f2f660d57d935fc65fbcd1d144
Gang Liang <gang.liang.2011@gmail.com>
2016-05-09 20:35:22 -0400

    python setupClass

--- quiz/lib/quiz.py
+++ quiz/lib/quiz.py
@@ -4,7 +4,6 @@ import simplejson
 import sys
 from pprint import pprint
 import logging
-
 import menu
 
 # time allocated - 2 hours
@@ -30,6 +29,18 @@ class QuizQA(Quiz):
         self.args = args
         self.kwargs = kwargs
 
+    # or use to init class directly
+    def init_args_quiz_inside_class():    
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument("-?", '--help', action="help",help='xxx')
+        parser.add_argument("-category", '--category', type=str, default=None, dest='category', help='category')
+
+        parser.add_argument("-run", '--run', action='store_true', dest='run', help='run')
+        parser.add_argument("-logging", '--logging', type=int, default=20, dest='logging', help='logging level 0 10 20')
+        parser.add_argument('-files', nargs='*')
+        args = parser.parse_args()
+        return args
+
     # arguments from self or (*args, **kwargs)
     def print_args(self,x, *args, **kwargs):
         # 
@@ -41,10 +52,30 @@ class QuizQA(Quiz):
             print arg
         print kwargs
 
+def init_args_quiz_outside_class():    
+
+    # argument may vary
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-?", '--help', action="help",help='xxx')
+    parser.add_argument("-category", '--category', type=str, default=None, dest='category', help='category')
+
+    parser.add_argument("-run", '--run', action='store_true', dest='run', help='run')
+    parser.add_argument("-logging", '--logging', type=int, default=20, dest='logging', help='logging level 0 10 20')
+    parser.add_argument('-files', nargs='*')
+    args = parser.parse_args()
+    return args
+
 def main():    
     # choose category
     categories = ['QC', 'python']
     category = menu.select_from_list(categories)
+    logger = logging.getLogger('quiz')
+    logger.addHandler(logging.StreamHandler() )
+    logger.setLevel(logging.DEBUG)
+    print logging.INFO
+    print logging.DEBUG
+    logger.info('1 - info')
+    logger.debug('2 - debug should not haapen as level is info')
     
     qz = Quiz(category)
     qz.print_args('ok')
---
+++ quiz/tests/test_quiz_setupclass.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import unittest
+import os
+import sys
+import mock
+import argparse
+pwd = os.path.dirname(os.path.realpath(__file__))
+base_dir = os.path.join(pwd,'..')
+sys.path.append(base_dir)
+
+# demo usage of args in unittest
+# demo of setupclass
+from lib import menu
+from lib import color_print
+
+def setUpModule():
+    pass
+def tearDownModule():
+    pass
+
+class QuizUnitTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print 'setup once'
+        cls._testdir = '/tmp/testdir'
+        cls._testfile = 'test.json'
+    @classmethod
+    def tearDownClass(cls):
+        cls._testdir = ''
+        cls._testfile =''
+
+class QuizQATest(QuizUnitTest):
+    @classmethod
+    def setUpClass(cls):
+        super(QuizQATest, cls).setUpClass()
+        cls._img = 'test.png'
+        print 'setup img once'
+
+    @classmethod
+    def tearDownClass(cls):
+        super(QuizQATest, cls).tearDownClass()
+        cls._img = ''
+
+    def setUp(self):
+        print 'setup'
+
+    def tearDown(self):
+        print 'tear donw'
+
+    def test_testdir(self):
+        self.assertEqual(self._testdir, '/tmp/testdir')
+
+    def test_testfile(self):
+        self.assertEqual(self._testfile, 'test.json')
+    def test_testimg(self):
+        self.assertEqual(self._img, 'test.png')
+
