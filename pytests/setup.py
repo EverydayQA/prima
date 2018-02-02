6da4797af52d77889760d9919c6bf9c0c83acb4e
EverydayQA <gang.liang.2011@gmail.com>
2018-02-01 22:49:57 -0500

    rename

---
+++ pytests/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='pytests',
+    version='0.1',
+    description='python tools',
+    url='https://github.com/EverydayQA',
+    author='Gang Liang',
+    author_email='gang.liang.2011@gmail.com',
+    license='MIT',
+    packages=['pytests'],
+    zip_safe=False)
