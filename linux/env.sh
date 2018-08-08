bdb3a34c1c7e60f8256ba1f3e7edccfb3cd29e0e
EverydayQA <gang.liang.2011@gmail.com>
2018-08-07 20:40:34 -0400

    add env.sh to manipulate env

--- linux/env.sh
+++ linux/env.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
-echo "TEST_VAR before export is: <$TEST_VAR>"
 
-export TEST_VAR=/opt/loca/netcdf
-echo "TEST_VAR after export is: <$TEST_VAR>"
-eval /sbin/ifconfig| awk '/inet addr/{print substr($2,6)}'
+PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/joe/.local/bin:/home/joe/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/joe/.local/bin:/home/joe/bin'
 
-result=''
-PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/gliang/.local/bin:/home/gliang/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/gliang/.local/bin:/home/gliang/bin'
-
-function split_envstr() {
-    # a envstr into array
-    IFS=$':'; set $1; for item; do echo $item; done; IFS=$oIFS;
-}
+function join { local IFS="$1"; shift; echo "$*"; }
 
 function remove_if_match() {
-    # remove python2.6 or python2.7 from pythonpath
-    echo 'remove_if_match'
-}
+    # uniq list of array without changing order
+    env_str=$1
+    match=$2
 
-function remove_items() {
-    # remove items from array
-    echo 'remove items'
+    declare -A items=()
+
+    IFS=$':'; set $1; 
+    for item; do 
+	if [[ $item = *$match* ]]; then
+	    continue
+	fi
+        items[$item]=1
+    done; 
+    IFS=$oIFS;
+
+    L=${!items[*]}
+    echo ${L// /:}
 }
-function join { local IFS="$1"; shift; echo "$*"; }
 
 function unique_str() {
     # uniq list of array without changing order
@@ -39,14 +39,18 @@ function unique_str() {
     echo ${L// /:}
 }
 
-function insert_first() {
-    # add array to array
-    echo 'insert_first'
+function append_front() {
+    echo $1:$2
+}
+
+function append_last() {
+    echo $2:$1
 }
 
 function array2envstr() {
     # array to envstr
-    echo 'array2envstr'
+    L=$1
+    echo ${L// /:}
 }
 
 function elementExists() {
@@ -74,10 +78,34 @@ function is_item_in() {
     if [[ ${array[$test1]} ]]; then echo "Exists"; fi    # Exists
     if [[ ${array[$test2]} ]]; then echo "Exists"; fi    # doesn't
 }
+
+function echo_var() {
+    echo $1 $2
+}
+
+function export_var() {
+    export $1=$2
+}
+
+path='PATH'
 echo $PATH
-# split
-r=$(split_envstr $PATH)
-echo $r
 PATH_NEW=$(unique_str $PATH)
-echo 'path-new'
-echo $PATH_NEW
+echo_var $path $PATH_NEW
+
+echo '1 str_to_be_append'
+FIRST=/home/joe/ven27p2u/bin
+PATH_NEW=$(append_front $FIRST $PATH_NEW)
+echo_var $path $PATH_NEW
+
+echo 'append_last'
+PATH_NEW=$(append_last $FIRST $PATH_NEW)
+echo_var $path $PATH_NEW
+
+echo 'remove items if match joe e.g python2.6'
+PATH_NEW=$(remove_if_match $PATH_NEW 'joe')
+echo_var $path $PATH_NEW
+
+echo 'export_var_value'
+export_var $path $PATH_NEW
+
+echo 'parameters opts parsing case switch to be added'
