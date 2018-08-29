ee60d6c8fc4cc25c8875307874498d95e441bc4e
EverydayQA <gang.liang.2011@gmail.com>
2018-08-29 19:35:54 -0400

    perl lint

--- perl_compile.pl
+++ perl_compile.pl
@@ -24,9 +24,25 @@ use utf8;
 use Cwd;
 # The OO way (recommended)
 use Test::Compile;
+use Test::More;
 my $test = Test::Compile->new();
 my $cwd = getcwd();
 my @dirs = ($cwd);
 $test->all_files_ok(@dirs);
-$test->done_testing();
+$test->all_pm_files_ok(@dirs);
+my @files = `find $cwd -name "*.pl"`;
+my @pms = `find $cwd -name "*.pm"`;
 
+for my $pfile(@pms){
+	chomp($pfile);
+	pm_file_compiles($pfile, $pfile);
+	pm_file_ok($pfile, $pfile);
+
+}
+
+for my $pfile(@files){
+	chomp($pfile);
+	pl_file_ok($pfile, $pfile);
+}
+
+$test->done_testing();
