86cbc7004260e360aaa90b8e71cd9a495d8d66e5
Gang Liang <gliang@localhost.localdomain>
2020-07-21 18:59:34 -0400

    update nested_dict

--- pytools/nested/nested_dict.py
+++ pytools/nested/nested_dict.py
@@ -1,85 +1,148 @@
-# from termcolor import cprint
+import collections
 
 
 class NestedDict(object):
     """
-    Before a nested dict in installed, this will be tested and used
     """
 
-    def set(self, value, *keys, **d):
+    def update(self, dnew={}, dinput={}):
         """
-        assume a simple set value
-        set to add or replace value with keys
-        # pop() the last item of the list, use your deepGet on it and set the popped off key on the resulting dict
+        recursive
         """
-        if not keys:
-            return d
-        # do not want to change the original d
-        vnow = self.get(*keys, **d)
-        if vnow == value:
-            return d
+        if not dnew:
+            return dinput
 
-        if len(keys) == 1:
-            # shallow
-            d[keys[0]] = value
-            return d
-        dprev = self.get(*keys[0:-1], **d)
-        # replace with lastkey
-        dprev[keys[-1]] = value
-        return d
+        if not isinstance(dnew, dict):
+            print('dnew is {}'.format(dnew))
+            raise Exception('update nested not dict')
 
-    def todo(self):
-        """
-        remove/reorder/reverse?
-        """
-        pass
+        # first level keys
+        for key in dnew.keys():
+            # updated dkey should automatically change d
+            v = dinput.get(key, None)
+            vnest = dnew.get(key, None)
+            if not isinstance(v, dict):
+                # value or not being set in original d
+                # set value to be value of dnew(or dnew_sub)
+                # the original v is replaced
+                dinput[key] = vnest
+                return dinput
 
-    def create(self, value, *keys):
-        """
-        create a single entry nested dict
-        """
-        dtmp = None
-        for key in reversed(keys):
-            if not dtmp:
-                dtmp = {key: value}
-                continue
-            dtmp = {key: dtmp}
-        return dtmp
+            if not isinstance(vnest, dict):
+                # replace existing dict with a value, loosing information
+                dinput[key] = vnest
+                return dinput
 
-    def get(self, *keys, **d):
+            vkey = self.update(dnew=vnest, dinput=v)
+            # replace the original value(v) with vkey
+            dinput[key] = vkey
+        return dinput
+
+    def update2(self, dnew={}, dinput={}):
         """
-        get the value with keys, default value is None?
-        return reduce(lambda d, k: d.get(k) if d else None, keys, sourceDict)
+        not recursive
         """
-        if not d:
+        if not dnew:
+            return dinput
+
+        if not isinstance(dnew, dict):
+            print('dnew is {}'.format(dnew))
+            raise Exception('update nested not dict')
+
+        deepkeys = self.deepset_keys(dnew)
+        if not deepkeys:
+            return self.merge_shallow(dnew=dnew, dinput=dinput)
+
+        vdeep = self.get(keys=deepkeys, dinput=dinput)
+        vdeep_new = self.get(keys=deepkeys, dinput=dnew)
+
+        if isinstance(vdeep_new, dict) and isinstance(vdeep, dict):
+            vmerge = self.merge_shallow(dnew=vdeep_new, dinput=vdeep)
+            return self.set(value=vmerge, keys=deepkeys, dinput=dinput)
+        return self.set(value=vdeep_new, keys=deepkeys, dinput=dinput)
+
+    def update4(self, dinput={}, dnew={}):
+        """
+        recursive
+        """
+        for key, value in dnew.iteritems():
+            if isinstance(value, collections.Mapping) and value:
+                returned = self.update4(dinput.get(key, {}), value)
+                dinput[key] = returned
+            else:
+                dinput[key] = dnew[key]
+        return dinput
+
+    def get(self, keys=[], dinput={}):
+        """
+        """
+        if not dinput:
             return None
 
-        value = None
-        count = 0
+        value = dinput
         for key in keys:
-            count = count + 1
-            if count == 1:
-                value = d.get(key, None)
-            else:
-                value = value.get(key, None)
-            # cannot continue
+            value = value.get(key, None)
+            if not value:
+                return value
             if not isinstance(value, dict):
-                if count == len(keys) - 1:
-                    raise Exception('keys is not right')
                 return value
         return value
 
-    def merge_shallow(self, dnew, **d):
+    def set(self, value=None, keys=[], dinput={}):
+        """
+        refactor -- for keys len from 1 to len(keys)
+        found -- keep going
+        not -- dnew  keys_left with values, d[the_key] = dnew
+        test and find the key to be used
+        test keys_left
+        test dnew
+        thekey point: keys.pop(0) or just pop()?
+        """
+        if not keys:
+            return dinput
+
+        vnow = self.get(keys=keys, dinput=dinput)
+        if vnow == value:
+            return dinput
+
+        if len(keys) == 1:
+            # shallow
+            dinput[keys[0]] = value
+            return dinput
+        dprev = self.get(keys=keys[0:-1], dinput=dinput)
+        # replace with lastkey
+        dprev[keys[-1]] = value
+        return dinput
+
+    def create(self, keys=[], value=None):
+        """
+        None recursive creation of nested dict
+        """
+        if not keys:
+            return {}
+
+        d = None
+        for key in reversed(keys):
+            if d is None:
+                d = {key: value}
+                continue
+            d = {key: d}
+        return d
+
+    def merge_shallow(self, dnew={}, dinput={}):
+        """
+        this will be replaced by {**d, **d2} in python3.8
+        """
         for key in dnew.keys():
-            v = d.get(key, None)
+            v = dinput.get(key, None)
             vnew = dnew.get(key, None)
             if v == vnew:
                 # no change
                 continue
-            d[key] = vnew
-        return d
+            dinput[key] = vnew
+        return dinput
 
-    def deepset_keys(self, **d):
+    def deepset_keys(self, d):
         """
         single key at each level
         """
@@ -94,83 +157,3 @@ class NestedDict(object):
                 if v.keys() > 1:
                     return keys
         return keys
-
-    def shallow_setstar(self, key, value, **d):
-        """
-        if d is not to be changed outside this func, use this
-        """
-        d[key] = value
-        return d
-
-    def shallow_set(self, key, value, d):
-        """
-        if d is to be changed outside this func, use this way
-        """
-        d[key] = value
-        return d
-
-    def update2(self, dnew, **d):
-        """
-        update nested with a nested dict
-        assume dnew is a single entry for now, should be complicated
-        merge the deepest dict, not to replace
-        No need to do deepcopy with **d
-        the return d changed, but d is not from the place where d is provided as input
-        """
-        if not dnew:
-            return d
-
-        if not isinstance(dnew, dict):
-            print('dnew is {}'.format(dnew))
-            raise Exception('update nested not dict')
-            return d
-
-        deepkeys = self.deepset_keys(**dnew)
-        if not deepkeys:
-            return self.merge_shallow(dnew, **d)
-
-        vdeep = self.get(*deepkeys, **d)
-        vdeep_new = self.get(*deepkeys, **dnew)
-
-        if isinstance(vdeep_new, dict) and isinstance(vdeep, dict):
-            vmerge = self.merge_shallow(vdeep_new, **vdeep)
-            return self.set(vmerge, *deepkeys, **d)
-        return self.set(vdeep_new, *deepkeys, **d)
-
-    def update(self, dnew, **d):
-        """
-        update nested with a nested dict
-        assume dnew is a single entry for now, should be complicated
-        merge the deepest dict, not to replace
-        """
-        if not dnew:
-            return d
-
-        if not isinstance(dnew, dict):
-            print('dnew is {}'.format(dnew))
-            raise Exception('update nested not dict')
-            return d
-
-        # how to tell if 2 dict has the same depth?
-
-        # first level keys
-        for key in dnew.keys():
-            # updated dkey should automatically change d
-            v = d.get(key, None)
-            vnest = dnew.get(key, None)
-            if not isinstance(v, dict):
-                # value or not being set in original d
-                # set value to be value of dnew(or dnew_sub)
-                # the original v is replaced
-                d[key] = vnest
-                return d
-
-            if not isinstance(vnest, dict):
-                # replace existing dict with a value, loosing information
-                d[key] = vnest
-                return d
-
-            vkey = self.update(vnest, **v)
-            # replace the original value(v) with vkey
-            d[key] = vkey
-        return d
