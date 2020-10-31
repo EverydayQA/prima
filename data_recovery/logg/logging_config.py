60a7972ae7ecf811d09596388ce963f620a31b49
Gang Liang <gliang@localhost.localdomain>
2020-10-30 22:17:49 -0400

    finalize console_logger colorlog

--- data_recovery/logg/logging_config.py
+++ data_recovery/logg/logging_config.py
@@ -1,234 +1,158 @@
 import os
 import logging
 import logging.config
-import time
+import colorlog
 
 
-class ConstLoggingConfig(object):
+class LoggingCommon(object):
+
+    """
+    LOGGING-LEVEL in env for both logfile and console
+    each module could has the same logfile and logging-level of their own
+    but env logging-level is for every module of the session
+    """
+
     LOGGING_LEVEL_FILE = 'LOGGING_LEVEL_FILE'
     LOGGING_LEVEL_CONSOLE = 'LOGGING_LEVEL_CONSOLE'
     LOGGING_FILE = 'LOGGING_FILE'
-    INFO = 'INFO'
-    DEBUG = 'DEBUG'
-    WARN = 'WARN'
-    CRITICAL = 'CRITICAL'
-    ERROR = 'ERROR'
-
-
-class LoggingConfig(ConstLoggingConfig):
-
-    """
-    LOGGING_LEVEL_FILE
-    who is responsible to set LOGGING_LEVEL_CONSOLE env?
-    who will use the level?
-    what about multiple clis?
-    """
+    LOGFORMAT = "  %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
+    LOGFORMAT = "%(log_color)s - %(asctime)s - %(name)-12s - %(levelname)s - %(message)s",
 
     @property
     def format_simple(self):
+        # return self.LOGFORMAT
         return '%(name)-12s: %(levelname)-8s %(message)s'
 
     @property
     def format_verbose(self):
         return '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 
-    def d_level(self):
-        """
-        do not know how to set level as variable, add this
-        """
-        d = {}
-        d[logging.INFO] = self.INFO
-        d[logging.DEBUG] = self.DEBUG
-        d[logging.WARN] = self.WARN
-        d[logging.CRITICAL] = self.CRITICAL
-        d[logging.ERROR] = self.ERROR
-        return d
+    def get_logfile_xxxxlevelxxx(self):
+        return self.getenv_logging_level(self.LOGGING_LEVEL_FILE)
 
-    def console_only_config(self):
-        """
-        NOT to set env level
-        """
-        level_env = self.get_console_level()
-        if level_env:
-            level = int(level_env)
-        else:
-            # not to set env
-            # level = self.set_env_console_level(str(level))
-            # but do set logger level of the module
-            # default LEVEL, not too much info
-            level = logging.WARN
-
-        d = self.d_level()
-        levelstr = d.get(level, logging.WARN)
-
-        LOGGING = {
-            'version': 1,
-            'disable_existing_loggers': False,
-            'formatters': {
-                'console_format': {
-                    'format': self.format_simple, }, },
-            'handlers': {
-                'console': self.console_handler(levelstr), },
-
-            'root': {
-                'level': levelstr,
-                'handlers': ['console'], },
-        }
-        return LOGGING
-
-    def file_only_config(self, level=logging.DEBUG, logfile=None):
-        if not logfile:
-            # have to be special name
-            # raise exception?
-            logfile = '/tmp/logfile'
-
-        d = self.d_level()
-        levelstr = d.get(level, logging.DEBUG)
-
-        LOGGING = {
-            'version': 1,
-            'disable_existing_loggers': False,
-            'formatters': {
-                'file_format': {
-                    'format': self.format_verbose, }, },
-            'handlers': {
-                'logfile': self.logfile_handler(logfile), },
-
-            'root': {
-                'level': levelstr,
-                'handlers': ['logfile'], },
-        }
-        return LOGGING
-
-    def both_config(self, level=logging.DEBUG, logfile=None):
-        if not logfile:
-            logfile = '/tmp/logfile'
-
-        d = self.d_level()
-        levelstr = d.get(level, logging.DEBUG)
-
-        LOGGING = {
-            'version': 1,
-            'disable_existing_loggers': False,
-            'formatters': {
-                'console_format': {
-                    'format': self.format_simple, },
-                'file_format': {
-                    'format': self.format_verbose, }, },
-            'handlers': {
-                'console': self.console_handler(levelstr),
-                'logfile': self.logfile_handler(logfile), },
-
-            'root': {
-                'level': 'INFO',
-                'handlers': ['console', 'logfile'], },
-        }
-        return LOGGING
-
-    def console_handler(self, levelstr):
-        d = {'class': 'logging.StreamHandler',
-             'level': levelstr,
-             'formatter': 'console_format', }
-        return d
-
-    def logfile_handler(self, logfile):
-        d = {'class': 'logging.FileHandler',
-             'formatter': 'file_format',
-             'filename': logfile,
-             'mode': 'w'}
-        return d
-
-    def get_logfile_level(self):
+    def getenv_logging_level(self, field):
         """
         int
         """
-        level = os.environ.get(self.LOGGING_LEVEL_FILE)
+        level = os.environ.get(field)
         if level:
             return level
-        return self.set_level(logging.INFO)
+        return logging.INFO
 
-    def get_console_level(self):
-        """
-        str(int)
-        """
-        level = os.environ.get(self.LOGGING_LEVEL_CONSOLE)
-        if level:
-            return level
-        return None
+    def setenv_logging_level_file(self, level):
+        return self.setenv_logging_level(self.LOGGING_LEVEL_FILE,  level)
 
-    def set_env_logfile_level(self, level):
+    def setenv_logging_level(self, field, level):
         """
-        input: type str(int)
+        input: type int
         :rtype: int(str)
         """
         if not isinstance(level, int):
             print(level)
             raise Exception('set logging level must be int')
-        os.environ[self.LOGGING_LEVEL_FILE] = level
-        return int(level)
-
-    def set_env_console_level(self, level):
-        """
-        input: type str(int)
-        :rtype: int(str)
-        """
-        os.environ[self.LOGGING_LEVEL_CONSOLE] = str(level)
+        os.environ[field] = str(level)
         return int(level)
 
 
-def get_console_logger(name, level=None):
+class ConsoleLogging(LoggingCommon):
+
+    def get_color_console_logger(self, name):
+        level = self.use_logging_level()
+
+        formatter = colorlog.ColoredFormatter(
+            "%(log_color)s[%(asctime)s %(levelname)-8s%(module)s]%(reset)s "
+            "%(white)s%(message)s",
+            datefmt="%H:%M:%S",
+            reset=True,
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red',
+            })
+
+        handler = colorlog.StreamHandler()
+        handler.setFormatter(formatter)
+        handler.setLevel(level)
+
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        logger.addHandler(handler)
+        return logger
+
+    def console_logger(self, name):
+        """
+        dictconfig is not recommended, it has its own syntax
+        """
+        level = self.use_logging_level()
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(self.format_simple)
+        handler.setFormatter(formatter)
+        handler.setLevel(level)
+
+        logger = logging.getLogger(name)
+        logger.addHandler(handler)
+        logger.setLevel(level)
+
+        return logger
+
+    def use_logging_level(self):
+        level_env = self.getenv_logging_level_console()
+        if level_env:
+            return int(level_env)
+        # not to set env
+        # default LEVEL, not too much info
+        return logging.WARN
+
+    def getenv_logging_level_console(self):
+        """
+        str(int)
+        """
+        return self.getenv_logging_level(self.LOGGING_LEVEL_CONSOLE)
+
+    def setenv_logging_level_console(self, level):
+        """
+        input: type int
+        :rtype: int
+        """
+        return self.setenv_logging_level(self.LOGGING_LEVEL_CONSOLE, level)
+
+
+def console_logger(name, color=True, level=None):
     """
     The first cli will call this with level defined
     other modules will just call this func without level
     default: console logger
     """
-    lc = LoggingConfig()
+    cl = ConsoleLogging()
     if level and isinstance(level, int):
-        # set env
-        lc.set_env_console_level(level)
-    dc = lc.console_only_config()
-    logging.config.dictConfig(dc)
-    logging.warning('get_logger %s', time.asctime())
-    logger = logging.getLogger(name)
-    return logger
-
-
-def get_logger_both(name, level=logging.INFO, logfile=None):
-    """
-    This will use if env level if set
-    else logging.INFO
-    default: console logger
-    """
-    lc = LoggingConfig()
-    dc = lc.both_config(level=level, logfile='/tmp/helo')
-    logging.config.dictConfig(dc)
-    logging.warning('get_logger %s', time.asctime())
-    logger = logging.getLogger(name)
-    return logger
+        # set logging-level once at env
+        cl.setenv_logging_level_console(level)
+    if color is True:
+        return cl.get_color_console_logger(name)
+    return cl.console_logger(name)
 
 
 def main():
     # console only
-    logger = get_console_logger(__name__, level=logging.WARN)
+    logger = console_logger(__name__, color=True, level=logging.DEBUG)
     logger.warning('logger')
     logger.info('info')
     logger.debug('debug')
-    logger.warning('warn')
+    logger.warn('warn')
     logger.error('error')
-    lc = LoggingConfig()
-    logger.critical(lc.LOGGING_LEVEL_CONSOLE)
-    logger.critical(os.environ.get(lc.LOGGING_LEVEL_CONSOLE))
 
-    logger.critical(lc.d_level())
+    # for developping
+    cl = ConsoleLogging()
+    logger.critical(cl.LOGGING_LEVEL_CONSOLE)
+    logger.critical(os.environ.get(cl.LOGGING_LEVEL_CONSOLE))
+    logger.critical(cl.LOGGING_LEVEL_FILE)
+    logger.critical(os.environ.get(cl.LOGGING_LEVEL_FILE))
+    logger.critical(cl.LOGGING_FILE)
+    logger.critical(os.environ.get(cl.LOGGING_FILE))
+
     return
-
-    # not to use this way, use get_logger()
-    dc = lc.both_config(level=logging.INFO, logfile='/tmp/helo')
-    logging.config.dictConfig(dc)
-    logging.warning('The local time is %s', time.asctime())
-    logger = logging.getLogger(__name__)
-    logger = get_logger_both(__name__, logfile=None)
-    logger.critical('get_logger')
 
 
 if __name__ == '__main__':
